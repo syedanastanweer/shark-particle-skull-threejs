144970d19904b118d5bf0726ae4cff401f1b0a20
waleedamir571 <waleedamir063@gmail.com>
2024-08-19 11:57:45 -0700

    commit2

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,15 +1,30 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+const { merge } = require('webpack-merge');
+const path = require('path');
+const commonConfiguration = require('./webpack.common.js');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = merge(
     commonConfiguration,
     {
         mode: 'production',
         devtool: false,
-        plugins:
-        [
-            new CleanWebpackPlugin()
+        output: {
+            path: path.resolve(__dirname, '../public'), // Adjust the path if necessary
+            filename: '[name].[contenthash].js',
+            assetModuleFilename: 'assets/[hash][ext][query]' // for other static assets
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(glb|gltf)$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'static/models/gltf/[name][ext]' // Output the models in the correct directory
+                    }
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(), // Cleans the output directory before each build
         ]
     }
-)
+);
